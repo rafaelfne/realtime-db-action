b16061f1aa940d702d9b4410c6f4a01fe36513a7
Rafael Neves <106340904+rafaelwarren@users.noreply.github.com>
2023-01-31 08:07:12 -0300

    add operation execution

--- index.js
+++ index.js
@@ -5,16 +5,30 @@ const github = require('@actions/github');
 try {
     const credential = core.getInput('CREDENTIAL');
     const databaseURL = core.getInput('DATABASE_DEFAULT_URL');
+    const reference = core.getInput('REFERENCE');
+    const data = core.getInput('DATA');
+    const operation = core.getInput('OPERATION');
 
     const app = admin.initializeApp({
         credential,
         databaseURL
-    })
+    });
     
-    const database = app.database()
+    const database = app.database();
 
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    if (operation === 'get') {
+        database.ref(reference).once('value')
+        .then((value) => {
+            core.setOutput('RESULT', value.toJSON());
+        });
+    }
+
+    if (operation === 'set') {
+        database.ref(reference).set(data);
+    }
+
+    const payload = JSON.stringify(github.context.payload, undefined, 2);
     console.log(`The event payload: ${payload}`);
 } catch (error) {
-    core.setFailed(error.message)
+    core.setFailed(error.message);
 }
