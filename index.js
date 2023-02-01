4ae78eeefedf0da5892eeab153005760f07b4565
Rafael Neves <106340904+rafaelwarren@users.noreply.github.com>
2023-01-31 22:35:34 -0300

    separete firebase properties

--- index.js
+++ index.js
@@ -3,16 +3,37 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-    const credential = JSON.parse(core.getInput('CREDENTIAL'));
+    // const credential = JSON.parse(core.getInput('CREDENTIAL'));
     const databaseURL = core.getInput('DATABASE_DEFAULT_URL');
     const reference = core.getInput('REFERENCE');
     const data = JSON.parse(core.getInput('DATA'));
     const operation = core.getInput('OPERATION');
 
+    const project_id = core.getInput('PROJECT_ID');
+    const private_key = core.getInput('PRIVATE_KEY');
+    const private_key_id = core.getInput('PRIVATE_KEY_ID'); 
+    const client_email = cored.getInput('CLIENT_EMAIL');
+    const client_id = core.getInput('CLIENT_ID');
+    const auth_uri = core.getInput('AUTH_URI');
+    const token_uri = core.getInput('TOKEN_URI');
+    const auth_provider_x509_cert_url = core.getInput('AUTH_PROVIDER_X509_CERT_URL');
+    const client_x509_cert_url = core.getInput('CLIENT_X509_CERT_URL');
+
     core.debug(credential);
 
     const app = admin.initializeApp({
-        credential,
+        credential: {
+            type: 'service_account',
+            project_id,
+            private_key,
+            private_key_id,
+            client_email,
+            client_id,
+            auth_uri,
+            token_uri,
+            auth_provider_x509_cert_url,
+            client_x509_cert_url
+        },
         databaseURL
     });
     
